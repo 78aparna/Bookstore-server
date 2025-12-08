7f0b158cdac69eb331167acea9c9124e896a79b4
78aparna <aparnasunil620@gmail.com>
2025-12-08 19:16:59 +0530

    register completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const router = require('./Routes/routing')
+require('./config/db')
 
 //create server using express
 const bookstoreServer = express()
